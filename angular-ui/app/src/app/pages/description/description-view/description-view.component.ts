d5e58c8a6f762468b9c2cf919e7dbb4d1fc7cbf9
Ekaterina Shaustruk <es@banksiaglobal.com>
2024-01-08 00:45:16 +0100

    feat: add plan-page

--- angular-ui/app/src/app/pages/description/description-view/description-view.component.ts
+++ angular-ui/app/src/app/pages/description/description-view/description-view.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SetBtnComponent } from '../../../components/set-btn/set-btn.component';
 
 @Component({
   selector: 'app-description-view',
   standalone: true,
-  imports: [],
+  imports: [SetBtnComponent],
   templateUrl: './description-view.component.html',
   styleUrl: './description-view.component.scss'
 })
 export class DescriptionViewComponent {
+  public btnTitle = 'Make it real';
 
+  @Output() createPlan = new EventEmitter<any>();
 }
--- angular-ui/app/src/app/pages/description/description/description.component.ts
+++ angular-ui/app/src/app/pages/description/description/description.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
+import { DescriptionViewComponent } from '../description-view/description-view.component';
 
 @Component({
   selector: 'app-description',
   standalone: true,
-  imports: [],
-  templateUrl: './description.component.html',
-  styleUrl: './description.component.scss'
+  imports: [DescriptionViewComponent],
+  templateUrl: './description.component.html'
 })
-export class DescriptionComponent {
-
-}
+export class DescriptionComponent {}
