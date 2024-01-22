4471209a08bfd9b1f278c7ca58760921b88a2b5c
Ekaterina Shaustruk <es@banksiaglobal.com>
2024-01-22 23:39:43 +0100

    refactor: update page with user's results

--- angular-ui/app/src/app/pages/results/results-view/results-view.component.ts
+++ angular-ui/app/src/app/pages/results/results-view/results-view.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ICustomObservationResult } from '../../../share/interfaces/onservation.interface.';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { SetBtnComponent } from '../../../components/set-btn/set-btn.component';
 
 @Component({
   selector: 'app-results-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, SetBtnComponent],
   templateUrl: './results-view.component.html',
   styleUrl: './results-view.component.scss'
 })
-export class ResultsViewComponent {
+export class ResultsViewComponent implements OnInit {
+  ngOnInit(): void {
+    this.userCustomObservationData = [
+      {
+        name: 'Body Height',
+        code: 'string',
+        value: 'value',
+        date: '2024-01-22'
+      },
+      {
+        name: 'Blood pressure',
+        code: 'string',
+        value: 'value',
+        date: '2024-01-22'
+      },
+      {
+        name: 'Body Height',
+        code: 'string',
+        value: 'value',
+        date: '2024-01-22'
+      },
+      {
+        name: 'Blood pressure',
+        code: 'string',
+        value: 'value',
+        date: '2024-01-22'
+      }
+    ];
+  }
+  @Input() userCustomObservationData: ICustomObservationResult[] | null;
 
+  public btnTitle = 'Ok.Go Ahead';
+
+  @Output() goToMenu = new EventEmitter<any>();
 }
--- angular-ui/app/src/app/pages/results/results/results.component.ts
+++ angular-ui/app/src/app/pages/results/results/results.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ResultsViewComponent } from '../results-view/results-view.component';
+import { Router } from '@angular/router';
+import { CarePlanService } from '../../../share/services/care.service';
+import { LoadingService } from '../../../share/services/loading.service';
+import { LocalStorageService } from '../../../share/services/localStorage.service';
+import { ObservationService } from '../../../share/services/observation.service';
+import { Observable, map, of } from 'rxjs';
+import { ICustomObservationResult } from '../../../share/interfaces/onservation.interface.';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [ResultsViewComponent],
+  imports: [CommonModule, ResultsViewComponent],
   templateUrl: './results.component.html'
 })
-export class ResultsComponent {}
+export class ResultsComponent implements OnInit {
+  constructor(
+    public storage: LocalStorageService,
+    public careService: CarePlanService,
+    public observationService: ObservationService,
+    public loadingService: LoadingService,
+    private router: Router
+  ) {}
+
+  public user$: Observable<string>;
+  public userId: string;
+
+  public userCustomObservationData$: Observable<ICustomObservationResult[]>;
+
+  ngOnInit(): void {
+    this.getUserId();
+    this.loadUserObservationData();
+  }
+
+  private getUserId() {
+    this.userId = this.storage.getUserID();
+    this.user$ = of(this.userId);
+  }
+
+  private loadUserObservationData() {
+    const userObservationData$ =
+      this.observationService.getUserCustomObservationData(this.userId);
+    const loadData$ =
+      this.loadingService.showSpinnerUntilCompleted(userObservationData$);
+
+    this.userCustomObservationData$ = loadData$.pipe(map((data) => data));
+  }
+
+  public goToMenu() {
+    this.router.navigate(['/menu']);
+  }
+}
