224b0be08f91ba1807159ed14ed425b4eeda8703
Ekaterina Shaustruk <es@banksiaglobal.com>
2024-01-24 16:17:08 +0100

    refactor: add wrong data div

---
+++ angular-ui/app/src/app/components/wrong-data/wrong-data.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-wrong-data',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './wrong-data.component.html',
+  styleUrl: './wrong-data.component.scss'
+})
+export class WrongDataComponent {}
