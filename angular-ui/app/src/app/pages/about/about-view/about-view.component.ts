d6fd603a9292cbcdc6ce97439cc8d549eef7fc0d
Ekaterina Shaustruk <es@banksiaglobal.com>
2024-01-25 20:11:03 +0100

    fix: fix about page

--- angular-ui/app/src/app/pages/about/about-view/about-view.component.ts
+++ angular-ui/app/src/app/pages/about/about-view/about-view.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-about-view',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './about-view.component.html',
   styleUrls: ['./about-view.component.scss']
 })
